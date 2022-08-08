5a8ef7ad8b893c6f454ea011ccf7897be965d2bf
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-08-08 20:48:55 +0200

    [TASK] Tried to fix

--- src/main/kotlin/de/mathisburger/ccr/entities/BaseEntity.kt
+++ src/main/kotlin/de/mathisburger/ccr/entities/BaseEntity.kt
@@ -1,12 +1,15 @@
 package de.mathisburger.ccr.entities
 
 import java.util.*
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 abstract class BaseEntity(
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     var id: UUID = UUID.randomUUID()
 ) {
 
--- src/main/kotlin/de/mathisburger/ccr/services/SessionService.kt
+++ src/main/kotlin/de/mathisburger/ccr/services/SessionService.kt
@@ -11,7 +11,7 @@ class SessionService(val db: SessionRepository) {
      * Creates a new session
      */
     fun createSession(ip: String): Session? {
-        var session = Session(ip);
+        val session = Session(ip);
         db.save(session);
         return db.findSessionByIp(ip);
     }
